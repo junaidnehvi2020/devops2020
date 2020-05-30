beb58158abcc0e6a611586d81ddcec742d4b4dc1
junaidnehvi <junaid.nehvi@gmail.com>
2020-05-30 13:50:18 +0000

    added some more lines of code

--- test1.sh
+++ test1.sh
@@ -1,4 +1,6 @@
 #Creating shell script for Testing
 echo "Today's Date is"
-echo "###:   " `date`
+echo "########:   " `date`
+echo "Don't forget to brush your teeth"
+echo "########:   " `date`
 echo "Thanks for visiting here"
